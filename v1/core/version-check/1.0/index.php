6185aea44ce955d0aa48f92356551bc566fe7857
James Nylen <jnylen@gmail.com>
2018-10-09 20:36:42 -0500

    API response fixes:
    - Format `offers` as an array of objects
    - Change `upgrade` to `latest` (so that ClassicPress doesn't think there
      is an upgrade available)

--- v1/core/version-check/1.0/index.php
+++ v1/core/version-check/1.0/index.php
@@ -2,22 +2,24 @@
 
 $json = [
     'offers' => [
-        'response' => 'upgrade',
-        'download' => 'https://downloads.classicpress.net/release/classicpress-1.0.0.zip',
-        'locale' => 'en_US',
-        'packages' => [
-            'full' => '',
-            'no_content' => '',
-            'new_bundled' => '',
-            'partial' => false,
-            'rollback' => false,
+        [
+            'response' => 'latest',
+            'download' => 'https://downloads.classicpress.net/release/classicpress-1.0.0.zip',
+            'locale' => 'en_US',
+            'packages' => [
+                'full' => '',
+                'no_content' => '',
+                'new_bundled' => '',
+                'partial' => false,
+                'rollback' => false,
+            ],
+            'current' => '1.0.0',
+            'version' => '1.0.0',
+            'php_version' => '5.6.0',
+            'mysql_version' => '5.0',
+            'new_bundled' => '1.0.0',
+            'partial_version' => false,
         ],
-        'current' => '1.0.0',
-        'version' => '1.0.0',
-        'php_version' => '5.6.0',
-        'mysql_version' => '5.0',
-        'new_bundled' => '1.0.0',
-        'partial_version' => false,
     ],
     'translations' => []
 ];
