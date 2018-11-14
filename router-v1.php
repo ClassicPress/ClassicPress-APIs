ceb3cf65bb58b52d72d4776a60b3c5a0f6c5969c
James Nylen <jnylen@gmail.com>
2018-11-14 02:51:46 -0500

    Local dev improvement

--- router-v1.php
+++ router-v1.php
@@ -5,10 +5,18 @@
 $file = __DIR__ . '/v1' . $_SERVER['REQUEST_URI'];
 $index = $file . '/index.php';
 if (is_dir($file) && file_exists($index)) {
+	// Add trailing / if not already present
+	if (!preg_match('#/$#', $file)) {
+		header('HTTP/1.1 302 Found');
+		header("Location: $_SERVER[REQUEST_URI]/");
+		die();
+	}
+	// Load index.php file
 	require $index;
 } else if (preg_match('#\.json$#', $file) && file_exists($file)) {
 	header('Content-Type: application/json');
 	readfile($file);
+	die();
 } else {
 	return false;
 }
