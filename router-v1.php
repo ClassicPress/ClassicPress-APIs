f8155022f2d3bf1684db8cc254e280cc5c112c69
Viktor Szépe <viktor@szepe.net>
2019-07-02 08:33:23 +0200

    Proper and secure cli-server router (#13)

--- router-v1.php
+++ router-v1.php
@@ -2,13 +2,18 @@
 
 // NOTE: DO NOT expose this script at a public URL!
 
+// Prevent usage in a webserver.
+if ('cli-server' !== php_sapi_name()) {
+	die('Development only');
+}
+
 $file = __DIR__ . '/v1' . $_SERVER['REQUEST_URI'];
 $index = $file . '/index.php';
 if (is_dir($file) && file_exists($index)) {
 	// Add trailing / if not already present
 	if (!preg_match('#/$#', $file)) {
 		header('HTTP/1.1 302 Found');
-		header("Location: $_SERVER[REQUEST_URI]/");
+		header("Location: ${_SERVER['REQUEST_URI']}/");
 		die();
 	}
 	// Load index.php file
