2c9a1d1033f2047565131dfe5a992eca9517742e
James Nylen <jnylen@gmail.com>
2019-10-24 20:11:18 +0000

    Enable migrating from WP 5.2.4

--- v1/migration/index.php
+++ v1/migration/index.php
@@ -19,10 +19,10 @@ echo json_encode( [
 	// WordPress versions allowed for migration.
 	'wordpress' => [
 		'min'   => '4.9.0',
-		'max'   => '5.2.3',
+		'max'   => '5.2.5',
 		'other' => [
 			'#^4\.9$#',
-			'#^5\.2\.4-(alpha|beta|rc)#i',
+			'#^5\.2\.5-(alpha|beta|rc)#i',
 			'#^5\.3-(alpha|beta|rc)#i',
 		],
 	],
