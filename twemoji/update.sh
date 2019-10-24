26f401e9f7f024f6e574c45eabb04c45448e35da
James Nylen <jnylen@gmail.com>
2019-10-24 22:57:13 +0000

    Use temporary files when updating Twemoji API responses
    This avoids the possibility of serving a partially written file.

--- twemoji/update.sh
+++ twemoji/update.sh
@@ -22,14 +22,16 @@ for spec in 6f3545b9:2/svg; do
 	commit="$(echo "$spec" | cut -d: -f1)"
 	subdir="$(echo "$spec" | cut -d: -f2)"
 	json_filename_base="$(echo "$commit")_$(echo "$subdir" | tr / _).json"
+	json_filename_tmp="$json_filename_base.tmp"
 	json_filename="../../v1/twemoji/$json_filename_base"
 	git checkout "$commit"
-	echo -n '[' > "$json_filename"
+	echo -n '[' > "$json_filename_tmp"
 	ls "$subdir" \
 		| sed 's#^#"#; s#$#"#' \
 		| tr '\n' ',' \
 		| sed 's#,$##' \
-		>> "$json_filename"
-	echo -n ']' >> "$json_filename"
-	echo "$json_filename_base"
+		>> "$json_filename_tmp"
+	echo -n ']' >> "$json_filename_tmp"
+	mv "$json_filename_tmp" "$json_filename"
+	echo "Generated $json_filename_base"
 done
