bdfecd51a060df05031df0013d566c690b849666
Viktor Szépe <viktor@szepe.net>
2020-02-03 15:51:50 +0100

    Shellify update.sh
    Shorter & more robust.

--- twemoji/update.sh
+++ twemoji/update.sh
@@ -7,9 +7,10 @@ cd "$(dirname "$0")"
 # Create git repository if needed
 if [ ! -d twemoji ] || [ ! -d twemoji/.git ]; then
 	mkdir -p twemoji
-	pushd twemoji > /dev/null
+	(
+		cd twemoji/
 		git clone https://github.com/twitter/twemoji .
-	popd > /dev/null
+	)
 fi
 
 cd twemoji
@@ -19,19 +20,20 @@ git fetch origin
 
 # Generate each of the JSON file listings we need
 for spec in 6f3545b9:2/svg; do
-	commit="$(echo "$spec" | cut -d: -f1)"
-	subdir="$(echo "$spec" | cut -d: -f2)"
-	json_filename_base="$(echo "$commit")_$(echo "$subdir" | tr / _).json"
+	commit="$(echo "$spec" | cut -d ":" -f 1)"
+	subdir="$(echo "$spec" | cut -d ":" -f 2)"
+	json_filename_base="$(echo "$commit")_$(echo "$subdir" | tr "/" "_").json"
 	json_filename_tmp="$json_filename_base.tmp"
 	json_filename="../../v1/twemoji/$json_filename_base"
 	git checkout "$commit"
-	echo -n '[' > "$json_filename_tmp"
-	ls "$subdir" \
-		| sed 's#^#"#; s#$#"#' \
-		| tr '\n' ',' \
-		| sed 's#,$##' \
-		>> "$json_filename_tmp"
-	echo -n ']' >> "$json_filename_tmp"
+	{
+		echo -n '['
+		ls "$subdir" \
+			| sed -e 's#^.*$#"&"#' \
+			| tr '\n' ',' \
+			| sed -e 's#,$##'
+		echo -n ']'
+	} >"$json_filename_tmp"
 	mv "$json_filename_tmp" "$json_filename"
 	echo "Generated $json_filename_base"
 done
