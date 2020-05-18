034af2febd7ce3ef6fad94c0f925f6d39b10e07b
Wade <wade@striebel.ca>
2020-05-18 10:20:36 -0400

    Support 5.4.1
    Tested in the video posted here: 
    https://forums.classicpress.net/t/cant-switch-to-cp-with-plugin/2331/11?u=wadestriebel

--- v1/migration/index.php
+++ v1/migration/index.php
@@ -19,7 +19,7 @@ echo json_encode( [
 	// WordPress versions allowed for migration.
 	'wordpress' => [
 		'min'   => '4.9.0',
-		'max'   => '5.3.2',
+		'max'   => '5.4.1',
 		'other' => [
 			'#^4\.9$#',
 			'#^5\.3\.3-(alpha|beta|rc)#i',
