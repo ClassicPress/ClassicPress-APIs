13ad6dcd234756a0a49c76359cd177ac764aab9a
James Nylen <jnylen@gmail.com>
2021-10-03 17:57:51 +0000

    Fetch petitions until finding 10 items
    In 'order=latest' mode (for the Trending tab in ClassicPress), we throw
    out any petitions that have zero votes, because these are most likely
    petitions that have been bumped by the forum system without anyone
    commenting on them.
    
    This may cause the first page of Discourse results to have less than 10
    valid petitions in this mode, so fetch more pages until we get 10 items
    that can be returned in the API result.

--- bin/update-petitions-data.php
+++ bin/update-petitions-data.php
@@ -14,9 +14,13 @@ if (php_sapi_name() !== 'cli') {
 /**
  * Return a URL for petitions, in JSON (API) or HTML (webpage) format.
  */
-function get_petitions_url($order, $json = true) {
+function get_petitions_url($order, $json = true, $page = 0) {
 	$base_url = 'https://forums.classicpress.net/c/governance/petitions/77';
-	return $base_url . ($json ? '.json' : '') . '?order=' . $order;
+	$list_url = $base_url . ($json ? '.json' : '') . '?order=' . $order;
+	if ($page) {
+		$list_url .= "&page=$page";
+	}
+	return $list_url;
 }
 
 /**
@@ -25,8 +29,9 @@ function get_petitions_url($order, $json = true) {
  * @see https://docs.discourse.org/#operation/listCategoryTopics
  * @see https://github.com/ClassicPress/ClassicPress-APIs/issues/28
  */
-function list_petitions($order) {
-	$fch = curl_init(get_petitions_url($order));
+function list_petitions($order, $page = 0) {
+	$url = get_petitions_url($order, true, $page);
+	$fch = curl_init($url);
 	curl_setopt($fch, CURLOPT_RETURNTRANSFER, true);
 	$response = curl_exec($fch);
 	$response = json_decode($response, true);
@@ -54,75 +59,83 @@ $override_created_at = json_decode(
 
 // The main loop for this script.
 foreach (['votes', 'latest', 'created'] as $order) {
-	$petitions = list_petitions($order);
-	if (isset($petitions['error'])) {
-		die("Error listing petitions by order=$order: $petitions[error]");
-	}
+	$petitions_page = 0;
 	$results = [
 		'data' => [],
 		'link' => get_petitions_url($order, false),
 	];
-	$users_by_id = [];
-	foreach ($petitions['users'] as $user) {
-		$users_by_id[$user['id']] = $user;
-	}
-	foreach ($petitions['topic_list']['topics'] as $topic) {
-		$op_id = $topic['posters'][0]['user_id'];
-		$op_username = $users_by_id[$op_id]['username'];
-		/**
-		 * ClassicPress expects the petition status field to be one of 'open',
-		 * 'planned', or 'started' (completed petitions are currently not shown
-		 * in the dashboard). The data is a bit messy, so decide based on a
-		 * combination of the tags and whether the petition's thread is still
-		 * open.
-		 */
-		$petition_status = $topic['closed'] ? 'closed' : 'open';
-		foreach ($topic['tags'] as $tag) {
-			switch ($tag) {
-				case 'planned':
-					if ($petition_status !== 'started') {
-						$petition_status = 'planned';
-					}
-					break;
-				case 'pull-request':
-				case 'cp-research-plugin':
-					$petition_status = 'started';
-					break;
+
+	do {
+		$petitions = list_petitions($order, $petitions_page);
+		if (isset($petitions['error'])) {
+			die("Error listing petitions by order=$order: $petitions[error]");
+		}
+		$users_by_id = [];
+		foreach ($petitions['users'] as $user) {
+			$users_by_id[$user['id']] = $user;
+		}
+		foreach ($petitions['topic_list']['topics'] as $topic) {
+			$op_id = $topic['posters'][0]['user_id'];
+			$op_username = $users_by_id[$op_id]['username'];
+			/**
+			 * ClassicPress expects the petition status field to be one of
+			 * 'open', 'planned', or 'started' (completed petitions are
+			 * currently not shown in the dashboard). The data is a bit messy,
+			 * so decide based on a combination of the tags and whether the
+			 * petition's thread is still open.
+			 */
+			$petition_status = $topic['closed'] ? 'closed' : 'open';
+			foreach ($topic['tags'] as $tag) {
+				switch ($tag) {
+					case 'planned':
+						if ($petition_status !== 'started') {
+							$petition_status = 'planned';
+						}
+						break;
+					case 'pull-request':
+					case 'cp-research-plugin':
+						$petition_status = 'started';
+						break;
+				}
+			}
+			if ($petition_status === 'closed') {
+				continue;
+			}
+			/**
+			 * Skip 'order=latest' petitions with zero votes. This happens
+			 * because the petitions forum is set to automatically bump a
+			 * random topic every so often, and many of these topics have no
+			 * new activity since the petitions moved to the forums.
+			 */
+			if ($order === 'latest' && $topic['vote_count'] === 0) {
+				continue;
+			}
+			$results['data'][] = [
+				'title' => $topic['title'],
+				/**
+				 * The 'description' field was previously present in this API
+				 * response as an excerpt of the petition text, but this field
+				 * is not used by ClassicPress and the post text is not
+				 * returned by the Discourse API endpoint we are now using.
+				 */
+				'description'   => '',
+				'createdAt'     => $override_created_at[$topic['id']] ?? $topic['created_at'],
+				'createdBy'     => $op_username,
+				'votesCount'    => $topic['vote_count'],
+				'commentsCount' => $topic['reply_count'],
+				'status'        => $petition_status,
+				'tags'          => $topic['tags'],
+				'link'          => "https://forums.classicpress.net/t/$topic[slug]/$topic[id]",
+			];
+			if (count($results['data']) === 10) {
+				break;
 			}
 		}
-		if ($petition_status === 'closed') {
-			continue;
-		}
-		/**
-		 * Skip 'latest' petitions with zero votes. This happens because the
-		 * petitions forum is set to automatically bump a random topic every so
-		 * often, and many of these topics have no new activity since the
-		 * petitions moved to the forums.
-		 */
-		if ($order === 'latest' && $topic['vote_count'] === 0) {
-			continue;
-		}
-		$results['data'][] = [
-			'title' => $topic['title'],
-			/**
-			 * The 'description' field was previously present in this API
-			 * response as an excerpt of the petition text, but this field is
-			 * not used by ClassicPress and the post text is not returned by
-			 * the Discourse API endpoint we are now using.
-			 */
-			'description'   => '',
-			'createdAt'     => $override_created_at[$topic['id']] ?? $topic['created_at'],
-			'createdBy'     => $op_username,
-			'votesCount'    => $topic['vote_count'],
-			'commentsCount' => $topic['reply_count'],
-			'status'        => $petition_status,
-			'tags'          => $topic['tags'],
-			'link'          => "https://forums.classicpress.net/t/$topic[slug]/$topic[id]",
-		];
-		if (count($results['data']) === 10) {
-			break;
-		}
-	}
+
+		// 'For order=latest' we may need to fetch more pages of petitions.
+		$petitions_page++;
+	} while (count($results['data']) < 10);
+
 	// Write then move, to avoid the API code reading a partially written file.
 	file_put_contents(
 		__DIR__ . '/petitions-order-' . $order . '.json.tmp',
