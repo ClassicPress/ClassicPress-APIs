abc474bab5f26d8cbcbf8ffe7a0cb2dea69dfd42
nylen <nylen@users.noreply.github.com>
2022-04-01 01:36:29 +0000

    Update API for ClassicPress 1.4.0 release

--- v1/migration/index.php
+++ v1/migration/index.php
@@ -7,8 +7,8 @@ header( 'Content-Type: application/json' );
 
 // ClassicPress build info. See:
 // https://github.com/ClassyBot/ClassicPress-nightly/releases
-$build_version = '1.3.1';
-$build_date = '20211021';
+$build_version = '1.4.0';
+$build_date = '20220331';
 
 $version = "$build_version+migration.$build_date";
 $build_url = 'https://github.com/ClassyBot/ClassicPress-nightly'
