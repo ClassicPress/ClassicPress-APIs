39595d42f3e56108ad81adfbce717924df52f50f
Laurence Bahiirwa <laurence@mediauganda.com>
2022-07-18 18:59:34 +0300

    Add API endpoint for PHP version support.

---
+++ v1/core/support-check/1.0/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * REST API Endpoint used to check the recommended ClassicPress PHP version.
+ * This is similar to http://api.wordpress.org/core/serve-happy/1.0/ 
+ *
+ * Response should be an array with:
+ *  'recommended_version' - string - The PHP version recommended by ClassicPress.
+ *  'is_supported' - boolean - Whether the PHP version is actively supported.
+ *  'is_secure' - boolean - Whether the PHP version receives security updates.
+ *  'is_acceptable' - boolean - Whether the PHP version is still acceptable for ClassicPress.
+ */
+$json = [
+    'recommended_version' => '7.4',
+    'is_supported'        => true,
+    'is_secure'           => true,
+    'is_acceptable'       => true,
+];
+
+header('Content-Type: application/json');
+echo json_encode($json);
