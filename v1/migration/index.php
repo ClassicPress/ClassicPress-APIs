2714e7cc192f98a86d76e19ba2ada4c1b695d15a
mattyrob <mattyrob@users.noreply.github.com>
2023-08-09 01:28:43 +0000

    Update API for new WordPressPress Release

--- v1/migration/index.php
+++ v1/migration/index.php
@@ -19,10 +19,10 @@ echo json_encode( [
 	// WordPress versions allowed for migration.
 	'wordpress' => [
 		'min'   => '4.9.0',
-		'max'   => '6.2.2',
+		'max'   => '6.3',
 		'other' => [
 			'#^4\.9$#',
-			'#^6\.3-(alpha|beta|rc)#i',
+			'#^6\.4-(alpha|beta|rc)#i',
 		],
 	],
 	// ClassicPress build to use for migration.
