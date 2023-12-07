49c77ca0d5a71538182c4005a11a7ce35da5c83d
mattyrob <mattyrob@users.noreply.github.com>
2023-12-07 08:31:04 +0000

    Update API for new WordPress 6.4.2 Release

--- v1/migration/index.php
+++ v1/migration/index.php
@@ -19,7 +19,7 @@ echo json_encode( [
 	// WordPress versions allowed for migration.
 	'wordpress' => [
 		'min'   => '4.9.0',
-		'max'   => '6.4.1',
+		'max'   => '6.4.2',
 		'other' => [
 			'#^4\.9$#',
 			'#^6\.5-(alpha|beta|rc)#i',
